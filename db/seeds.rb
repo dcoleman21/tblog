15ebf7eb97f67c5b52f2499295a23ca8e6c495e1
Dani Coleman <danicoleman00@gmail.com>
2021-02-15 10:35:33 -0700

    Add a sample article into the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+article1 = Article.create!(title: "My First Article", body: "This is my first article")
