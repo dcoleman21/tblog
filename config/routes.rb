f33963f58247fc7b043a40873943462ddbb8b08e
Dani Coleman <danicoleman00@gmail.com>
2021-02-15 11:14:44 -0700

    Add route for articles/id to show action. Add route for articles/id/edit to edit action. Add route for articles/id to update action. Add route for articles/id to destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get '/articles', to: 'articles#index'
   get '/articles/new', to: 'articles#new'
   post '/articles', to: 'articles#create'
+  get '/articles/:id', to: 'articles#show'
+  get '/articles/:id/edit', to: 'articles#edit'
+  patch '/articles/:id', to: 'articles#update'
+  delete '/articles/:id', to: 'articles#destroy'
 end
