b559ebaff7fb5b08710fc98407e1ef290dbfbd7d
Dani Coleman <danicoleman00@gmail.com>
2021-02-15 10:34:42 -0700

    Edit article index spec to show articles and have a link to create a new article

--- spec/features/Articles/index_spec.rb
+++ spec/features/Articles/index_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "As a visitor" do
-  it "the welcome page displays a welcome message and form to create a new article" do
+RSpec.describe "Articles index page" do
+  it "the articles page displays a list of articles and a form to create a new article" do
+    article1 = Article.create!(title: "My First Article", body: "This is my first article")
+    article2 = Article.create!(title: "My Second Article", body: "This is my second article")
+
     visit root_path
-    expect(page).to have_content("Your Articles")
-    expect(page).to have_content("Create a New Article")
+
+    expect(page).to have_content('Your Articles')
+    expect(page).to have_content(article1.title)
+    expect(page).to have_content(article1.body)
+    expect(page).to have_link("Create a New Article")
   end
 end
