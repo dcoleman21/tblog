93aea47c25c279701243e258af2997e059406fb1
Dani Coleman <danicoleman00@gmail.com>
2021-02-15 11:27:26 -0700

    Update test to make the article title a link

--- spec/features/Articles/index_spec.rb
+++ spec/features/Articles/index_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Articles index page" do
-  it "the articles page displays a list of articles and a form to create a new article" do
+  it "the articles page displays a list of articles and a link to create a new article" do
     article1 = Article.create!(title: "My First Article", body: "This is my first article")
     article2 = Article.create!(title: "My Second Article", body: "This is my second article")
 
     visit root_path
 
     expect(page).to have_content('Your Articles')
-    expect(page).to have_content(article1.title)
+    expect(page).to have_link(article1.title)
     expect(page).to have_content(article1.body)
     expect(page).to have_link("Create a New Article")
   end
