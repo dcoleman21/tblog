b8a66487660ee58022370c6ed3a2e4fb4fafd45a
Dani Coleman <danicoleman00@gmail.com>
2021-02-15 11:15:06 -0700

    Add show, edit, update, destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,29 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article = Article.find(params[:id])
+    article.update(article_params)
+
+    article.save
+
+    redirect_to "/articles/#{article.id}"
+  end
+
+  def destroy
+    Article.destroy(params[:id])
+    redirect_to articles_path
+  end
+
+  private
 
   def article_params
     params.permit(:title, :body)
