f3e1640b75c67eb1c3f232910f15ac75111c3c7e
Dani Coleman <danicoleman00@gmail.com>
2021-02-15 10:37:35 -0700

    Add a new action and a create action passing in the article params; title and body.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,19 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
 
+  def new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save!
+    redirect_to articles_path
+  end
+
+
+  def article_params
+    params.permit(:title, :body)
   end
 end
